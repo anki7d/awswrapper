c80091db9a5993afd0bcc151c3fb2a05eaab2d45
anki7d <58514185+anki7d@users.noreply.github.com>
2020-03-04 10:53:34 +0530

    example use in CI3

---
+++ Welcome.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function upload()
+	{
+
+		$key = "AKIAYRCWOXENHDQUS7GN";
+		$secret = "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";
+
+		$this->load->library('aws');
+
+		$this->aws->set_auth_keys($key, $secret, 'us-east-1');
+
+		$this->aws->process_aws_auth();
+
+		$data = $this->aws->upload_s3_obj(
+			'wimtestbhav',
+			$filcontent = 'test',
+			'/storage/dump.txt'
+		);
+
+		print_r($data);
+
+		$data = $this->aws->copy_s3_obj(
+			'wimtestbhav',
+			$filcontent = 'test',
+			'/storage/dump.txt'
+		);
+
+		print_r($data);
+
+		$data = $this->aws->delete_s3_obj(
+			'wimtestbhav',
+			'/storage/dump.txt'
+		);
+
+		print_r($data);
+	}
+}
